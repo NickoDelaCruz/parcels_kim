003924978391fe62c07bb64920258a11887b1b9c
Kimberly Huynh and Nicko Dela Cruz <nicko@>
2018-02-12 15:27:40 -0800

    test and pass first spec

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,10 +1,14 @@
 class Parcel
+  def initialize(length, width, height)
+    @length = length
+    @width = width
+    @height = height
+  end
 
-
-
-
-
-
-
-
+# l x w x h = v
+  def volume1(length,width,height)
+    volume = length * width * height
+  end
 end
+
+# volume x price = price to ship
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -2,3 +2,12 @@ require('rspec')
 require('pry')
 require('sinatra')
 require('parcels')
+
+# specify dimensions & weight
+# call volume on an instance
+describe('Parcel') do
+it("calculates the volume of the package") do
+    volume = Parcel.new(3,3,3)
+    expect(volume.volume1(3,3,3)).to(eq(27))
+  end
+end
